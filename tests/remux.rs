6cd48cb933918bf2a257c1b93c9ea26c8f30c1ea
Egor Dmitriev <egordmitriev2@gmail.com>
2019-07-23 20:56:00 +0200

    Refactored litio to litaudioio

--- tests/remux.rs
+++ tests/remux.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod remux_tests {
 	use litaudio::{Dynamic, AudioDeinterleaved, U2};
-	use litio::*;
+	use litaudioio::*;
 	use std::error::Error;
 	use std::path::PathBuf;
 
@@ -10,7 +10,7 @@ mod remux_tests {
 		let mut in_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 		in_path.push( "assets/jazz.mp3");
 
-		let s: Result<AudioDeinterleaved<f32, U2, Dynamic>, litio::error::Error> = read_audio(in_path.as_path());
+		let s: Result<AudioDeinterleaved<f32, U2, Dynamic>, litaudioio::error::Error> = read_audio(in_path.as_path());
 
 		let mut out_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 		out_path.push("tmp/jazz.mp3");
